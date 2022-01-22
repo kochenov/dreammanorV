e52e8a7d702afd4028163d34b8b20116c4e6c91d
kochenov <kochenof@gmail.com>
2022-01-22 21:41:21 +0300

    Добавил форму рассчётов грядок

--- src/components/box/tab-panel/FormSeeding.vue
+++ src/components/box/tab-panel/FormSeeding.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="box__panel tab-active">
     <div class="calc-seeding">
+      <div v-if="message != ''">
+        <div class="alert" v-for="(text, index) in message" :key="index">
+          {{ text }}
+        </div>
+      </div>
       <form id="calc-veget" action="" method="get">
         <div class="form-input">
           <div class="grup">
             <label for="sort-tomat">Выбирите сорт томата</label
-            ><select id="sort-tomat" name="sort-tomat">
+            ><select
+              @change="getSortData"
+              id="sort-tomat"
+              name="sort-tomat"
+              v-model="currentSort"
+            >
               <option selected value="default">
                 Без сорта (Универсальные значения)
               </option>
-              <option value="Столыпин">Столыпин</option>
-              <option value="Томат 2">Томат 2</option>
-              <option value="Томат 3">Томат 3</option>
-              <option value="Томат 4">Томат 4</option>
-              <option value="Томат 5">Томат 5</option>
-              <option value="Томат 6">Томат 6</option>
+              <option v-for="item in sorts" :key="item.id" :value="item">
+                {{ item.name }}
+              </option>
             </select>
           </div>
           <div class="grup">
@@ -22,8 +29,11 @@
             ><input
               id="bushes"
               type="number"
-              value="1"
               min="1"
+              :class="{
+                error: bushes === '' && message !== '',
+              }"
+              v-model="bushes"
               placeholder="Сколько кустов"
             />
           </div>
@@ -32,7 +42,10 @@
             ><input
               id="rows"
               type="number"
-              value="1"
+              :class="{
+                error: rows === '' && message !== '',
+              }"
+              v-model="rows"
               requred
               min="1"
               placeholder="Сколько будет рядов"
@@ -47,6 +60,10 @@
               type="number"
               min="1"
               placeholder="70 см"
+              :class="{
+                error: distanceBetweenRows === '' && message !== '',
+              }"
+              v-model="distanceBetweenRows"
             />
           </div>
           <div class="grup">
@@ -56,16 +73,60 @@
               type="number"
               min="1"
               placeholder="100 см"
+              :class="{
+                error: distanceBetweenBushes === '' && message !== '',
+              }"
+              v-model="distanceBetweenBushes"
             />
           </div>
         </div>
         <div class="btn-group">
-          <button class="btn" id="calc-tomat-submit">Сделать расчёт</button>
+          <button
+            @click.prevent="submitToResult"
+            class="btn"
+            id="calc-tomat-submit"
+          >
+            Сделать расчёт
+          </button>
         </div>
       </form>
-      <div class="data-res-view">
-        <div id="rezult"></div>
-        <div class="btn-group hidden">
+      <div class="data-res-view" v-if="result">
+        <div id="rezult">
+          <div class="res-ogorod">
+            <div class="distance-between-rows">
+              <b
+                >Расстояние между рядов: {{ distanceBetweenRows }} см (
+                {{ distanceBetweenRows / 100 }} м )</b
+              >
+            </div>
+            <div class="distance-between-bushes">
+              <b
+                >Растояние между кустов: {{ distanceBetweenBushes }} см (
+                {{ distanceBetweenBushes / 100 }} м )</b
+              ><br />
+            </div>
+            <div class="width">
+              <b>Ширина грядки: ${width} см ( ${width / 100} м ) </b>
+            </div>
+            <div class="height">
+              <b>Длина грядки: ${height} см ( ${height / 100} м )</b>
+            </div>
+            <div class="perimetr">
+              <b>Перимитр: ${(2 * (width + height)) / 100} м</b>
+            </div>
+            <div class="ploshad">
+              <b
+                >Площадь: ${((width / 100) * (height / 100)).toFixed(1)} кв. м (
+                ${((((width / 100) * (height / 100))) * 0.01).toFixed(1)} соток
+                )
+              </b>
+            </div>
+            <div class="one-rows">
+              <b>В одном ряду: ${oneRows} кустов</b>
+            </div>
+          </div>
+        </div>
+        <div class="btn-group">
           <button class="btn" id="calc-tomat-save">Сохранить результат</button
           ><button class="btn" id="calc-tomat-plan">
             Запланировать посадку
@@ -76,3 +137,81 @@
     </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      sorts: [
+        {
+          id: 1,
+          name: "Столыпин",
+          distanceBetweenRows: 70,
+          distanceBetweenBushes: 100,
+        },
+        {
+          id: 2,
+          name: "Сорт 1",
+          distanceBetweenRows: 40,
+          distanceBetweenBushes: 40,
+        },
+        {
+          id: 3,
+          name: "Сорт 2",
+          distanceBetweenRows: 50,
+          distanceBetweenBushes: 60,
+        },
+        {
+          id: 4,
+          name: "Сорт 3",
+          distanceBetweenRows: 60,
+          distanceBetweenBushes: 70,
+        },
+      ],
+      currentSort: "default",
+      distanceBetweenRows: "",
+      distanceBetweenBushes: "",
+      bushes: 1,
+      rows: 1,
+      result: false,
+      message: "",
+    };
+  },
+  methods: {
+    getSortData() {
+      if (this.currentSort !== "default") {
+        this.distanceBetweenRows = this.currentSort.distanceBetweenRows;
+        this.distanceBetweenBushes = this.currentSort.distanceBetweenBushes;
+      }
+    },
+    submitToResult() {
+      if (
+        this.distanceBetweenRows !== "" &&
+        this.distanceBetweenBushes !== "" &&
+        this.bushes !== "" &&
+        this.rows !== ""
+      ) {
+        if (!this.result) {
+          this.result = true;
+          this.message = "";
+        }
+      } else {
+        this.message = ["Пожалуйста, заполните все поля формы!"];
+        this.result = false;
+      }
+    },
+  },
+};
+</script>
+<style lang="scss">
+.alert {
+  color: red;
+  padding: 15px 30px;
+  border: 1px solid red;
+  border-radius: 3px;
+  background-color: #fff;
+  margin-bottom: 15px;
+}
+.error {
+  border: 1px solid red;
+}
+</style>
