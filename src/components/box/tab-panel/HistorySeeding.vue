ac9b8bc8feb66e1c9fe6848c909d26648f1ad138
kochenov <kochenof@gmail.com>
2022-01-23 23:47:48 +0300

    Добавил прототип истории расчётов

--- src/components/box/tab-panel/HistorySeeding.vue
+++ src/components/box/tab-panel/HistorySeeding.vue
@@ -1,5 +1,63 @@
 <template>
   <div class="box__panel tab-active">
+    <div class="history-seeding">
+      <div class="item">
+        <div class="title">
+          <h3>Грядка в полесаднике рядом со входом слева от калитки</h3>
+          <span class="date-history">11 Января 2022</span>
+        </div>
+        <div class="meta">
+          <div class="product-name ticket">Овощь: <span>Томат</span></div>
+          <div class="sort ticket">Сорт: <span>Столыпин</span></div>
+          <div class="bushes ticket">
+            Количество кустов: <span>15 шт.</span>
+          </div>
+          <div class="todgge">
+            <button>
+              <fa icon="arrow-right" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="title">
+          <h3>Грядка в полесаднике рядом со входом слева от калитки</h3>
+          <span class="date-history">11 Января 2022</span>
+        </div>
+        <div class="meta">
+          <div class="product-name ticket">Овощь: <span>Томат</span></div>
+          <div class="sort ticket">Сорт: <span>Столыпин</span></div>
+          <div class="bushes ticket">
+            Количество кустов: <span>15 шт.</span>
+          </div>
+          <div class="todgge">
+            <button>
+              <fa icon="arrow-right" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="title">
+          <h3>Грядка в полесаднике рядом со входом слева от калитки</h3>
+          <span class="date-history">11 Января 2022</span>
+        </div>
+        <div class="meta">
+          <div class="product-name ticket">Овощь: <span>Томат</span></div>
+          <div class="sort ticket">Сорт: <span>Столыпин</span></div>
+          <div class="bushes ticket">
+            Количество кустов: <span>15 шт.</span>
+          </div>
+          <div class="todgge">
+            <button>
+              <fa icon="arrow-right" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
     <h3 class="box__panel-title">
       Зачем нужна история расчётов грядок для томатов?
     </h3>
@@ -27,3 +85,48 @@
     </div>
   </div>
 </template>
+<style scoped lang="scss">
+.history-seeding {
+  .title,
+  .meta {
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 0;
+  }
+  .meta {
+    justify-content: start;
+    align-items: center;
+    gap: 20px;
+  }
+  .ticket {
+    border: 1px solid green;
+    padding: 4px 10px;
+    font-size: 10px;
+    text-transform: uppercase;
+    font-weight: 600;
+    background: #194484;
+    color: #ffffff;
+  }
+  .item {
+    margin: 0 0 40px 0;
+    border: 1px solid #06b25f;
+    padding: 10px 15px;
+  }
+  .todgge {
+    margin-left: auto;
+    button {
+      cursor: pointer;
+      padding: 1px 10px;
+      border: 2px solid #06b25f;
+      background: #f0f0f000;
+      color: #06b25f;
+      box-shadow: none;
+      &:hover {
+        border: 2px solid #000000;
+        background: #f0f0f000;
+        color: #000000;
+      }
+    }
+  }
+}
+</style>
