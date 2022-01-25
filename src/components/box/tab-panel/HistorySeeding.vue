ed1a11f826159ffcb1f98f217193137b1c4c1013
kochenov <kochenof@gmail.com>
2022-01-25 11:28:01 +0300

    Добавил пример истории расчётов размеров грядок

--- src/components/box/tab-panel/HistorySeeding.vue
+++ src/components/box/tab-panel/HistorySeeding.vue
@@ -3,54 +3,65 @@
     <div class="history-seeding">
       <div v-for="item in history" :key="item.id" class="item">
         <div class="title">
-          <h3>Грядка в полесаднике рядом со входом слева от калитки</h3>
-          <span class="date-history">11 Января 2022</span>
+          <h3>{{ item.title }}</h3>
+          <span class="date-history">{{ item.dateAdd }}</span>
         </div>
         <div class="meta">
-          <div class="product-name ticket">Овощь: <span>Томат</span></div>
-          <div class="sort ticket">Сорт: <span>Столыпин</span></div>
+          <div class="product-name ticket">
+            <span>{{ item.vegetable }}</span>
+          </div>
+          <div class="sort ticket">
+            Сорт: <span>{{ item.sort }}</span>
+          </div>
           <div class="bushes ticket">
-            Количество кустов: <span>15 шт.</span>
+            Кустов: <span>{{ item.bushes }} шт.</span>
+          </div>
+          <div class="bushes ticket">
+            Рядов: <span>{{ item.rows }} шт.</span>
           </div>
           <div class="todgge">
-            <button>
-              <fa icon="arrow-right" />
+            <button @click="item.exp = !item.exp">
+              <fa v-if="!item.exp" icon="arrow-right" />
+              <fa v-if="item.exp" icon="arrow-down" />
             </button>
           </div>
         </div>
-        <div class="content">
+        <div v-show="item.exp" class="content">
           <div class="res-ogorod">
             <div class="row">
               <div>Расстояние между рядов:</div>
               <div>
-                {{ distanceBetweenRows }} см ( {{ distanceBetweenRows / 100 }} м
-                )
+                {{ item.distanceBetweenRows }} см (
+                {{ item.distanceBetweenRows / 100 }} м )
               </div>
             </div>
             <div class="row">
               <div>Растояние между кустов:</div>
               <div>
-                {{ distanceBetweenBushes }} см (
-                {{ distanceBetweenBushes / 100 }} м )
+                {{ item.distanceBetweenBushes }} см (
+                {{ item.distanceBetweenBushes / 100 }} м )
               </div>
             </div>
             <div class="row">
               <div>Ширина грядки:</div>
-              <div>{{ width }} см ( {{ width / 100 }} м )</div>
+              <div>{{ item.width }} см ( {{ item.width / 100 }} м )</div>
             </div>
             <div class="row">
               <div>Длина грядки:</div>
-              <div>{{ height }} см ( {{ height / 100 }} м )</div>
+              <div>{{ item.height }} см ( {{ item.height / 100 }} м )</div>
             </div>
             <div class="row">
               <div>Перимитр:</div>
-              <div>{{ (2 * (width + height)) / 100 }} м</div>
+              <div>{{ (2 * (item.width + item.height)) / 100 }} м</div>
             </div>
             <div class="row">
               <div>Площадь:</div>
               <div>
-                {{ ((width / 100) * (height / 100)).toFixed(1) }} кв. м (
-                {{ ((width / 100) * (height / 100) * 0.01).toFixed(1) }}
+                {{ ((item.width / 100) * (item.height / 100)).toFixed(1) }} кв.
+                м (
+                {{
+                  ((item.width / 100) * (item.height / 100) * 0.01).toFixed(1)
+                }}
                 соток )
               </div>
             </div>
@@ -68,7 +79,38 @@
 export default {
   data() {
     return {
-      history: [{ id: 1 }, { id: 2 }],
+      history: [
+        {
+          id: 1,
+          exp: false,
+          title: "Грядка в полесаднике рядом со входом слева от калитки",
+          vegetable: "Томаты",
+          bushes: 15,
+          sort: "Столыпин",
+          rows: 2,
+          distanceBetweenRows: 60,
+          distanceBetweenBushes: 50,
+          oneRows: 7,
+          width: 1400,
+          height: 1800,
+          dateAdd: "21 Января 2020",
+        },
+        {
+          id: 2,
+          exp: false,
+          title: "Грядка у уличного туалета в тени от сарая слева",
+          vegetable: "Огурцы",
+          bushes: 25,
+          sort: "Пальчик",
+          rows: 5,
+          distanceBetweenRows: 60,
+          distanceBetweenBushes: 50,
+          oneRows: 7,
+          width: 1300,
+          height: 1700,
+          dateAdd: "22 Января 2020",
+        },
+      ],
     };
   },
 };
