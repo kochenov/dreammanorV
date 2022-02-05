bdc2a5cb8902e55b2e8dd4718b9ce82b560f05c0
kochenov <kochenof@gmail.com>
2022-02-05 23:05:08 +0300

    Добавлены вычисления результатов

--- src/components/box/tab-panel/HistorySeeding.vue
+++ src/components/box/tab-panel/HistorySeeding.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="box__panel tab-active">
+    <div v-if="errorDataApi">
+      <div class="alert">Ошибка загрузки данных!</div>
+    </div>
+    <div v-if="loadingDataApi" class="alert loading">Загрузка данных ...</div>
     <div class="history-seeding">
       <div class="history-seeding__inform">
         <div class="todgge">
@@ -14,15 +18,15 @@
       </div>
       <div v-for="item in history" :key="item.id" class="item">
         <div class="title">
-          <h3>{{ item.title }}</h3>
+          <h3>{{ item.name }}</h3>
           <span class="date-history">{{ item.dateAdd }}</span>
         </div>
         <div class="meta">
           <div class="product-name ticket">
-            <span>{{ item.vegetable }}</span>
+            <span>{{ item.vegetables.vegetable.name }}</span>
           </div>
           <div class="sort ticket">
-            Сорт: <span>{{ item.sort }}</span>
+            Сорт: <span>{{ item.vegetables.name }}</span>
           </div>
           <div class="bushes ticket">
             Кустов: <span>{{ item.bushes }} шт.</span>
@@ -35,10 +39,6 @@
               <fa v-if="!item.exp" icon="arrow-right" />
               <fa v-if="item.exp" icon="arrow-down" />
             </button>
-            <button @click="item.exp = !item.exp">
-              <fa v-if="!item.exp" icon="arrow-right" />
-              <fa v-if="item.exp" icon="arrow-down" />
-            </button>
           </div>
         </div>
         <div v-show="item.exp" class="content">
@@ -46,43 +46,92 @@
             <div class="row">
               <div>Расстояние между рядов:</div>
               <div>
-                {{ item.distanceBetweenRows }} см (
-                {{ item.distanceBetweenRows / 100 }} м )
+                {{ item.vegetables.distanceBetweenRows }} см (
+                {{ item.vegetables.distanceBetweenRows / 100 }} м )
               </div>
             </div>
             <div class="row">
               <div>Растояние между кустов:</div>
               <div>
-                {{ item.distanceBetweenBushes }} см (
-                {{ item.distanceBetweenBushes / 100 }} м )
+                {{ item.vegetables.distanceBetweenBushes }} см (
+                {{ item.vegetables.distanceBetweenBushes / 100 }} м )
               </div>
             </div>
             <div class="row">
               <div>Ширина грядки:</div>
-              <div>{{ item.width }} см ( {{ item.width / 100 }} м )</div>
+              <div>
+                {{ widh(item.rows, item.vegetables.distanceBetweenRows) }} см (
+                {{ widh(item.rows, item.vegetables.distanceBetweenRows) / 100 }}
+                м )
+              </div>
             </div>
             <div class="row">
               <div>Длина грядки:</div>
-              <div>{{ item.height }} см ( {{ item.height / 100 }} м )</div>
+              <div>
+                {{
+                  height(
+                    oneRows(item.bushes, item.rows),
+                    item.vegetables.distanceBetweenBushes
+                  )
+                }}
+                см (
+                {{
+                  height(
+                    oneRows(item.bushes, item.rows),
+                    item.vegetables.distanceBetweenBushes
+                  ) / 100
+                }}
+                м )
+              </div>
             </div>
             <div class="row">
               <div>Перимитр:</div>
-              <div>{{ (2 * (item.width + item.height)) / 100 }} м</div>
+              <div>
+                {{
+                  (2 *
+                    (widh(item.rows, item.vegetables.distanceBetweenRows) +
+                      height(
+                        oneRows(item.bushes, item.rows),
+                        item.vegetables.distanceBetweenBushes
+                      ))) /
+                  100
+                }}
+                м
+              </div>
             </div>
             <div class="row">
               <div>Площадь:</div>
               <div>
-                {{ ((item.width / 100) * (item.height / 100)).toFixed(1) }} кв.
-                м (
                 {{
-                  ((item.width / 100) * (item.height / 100) * 0.01).toFixed(1)
+                  (
+                    (widh(item.rows, item.vegetables.distanceBetweenRows) /
+                      100) *
+                    (height(
+                      oneRows(item.bushes, item.rows),
+                      item.vegetables.distanceBetweenBushes
+                    ) /
+                      100)
+                  ).toFixed(1)
+                }}
+                кв. м (
+                {{
+                  (
+                    (widh(item.rows, item.vegetables.distanceBetweenRows) /
+                      100) *
+                    (height(
+                      oneRows(item.bushes, item.rows),
+                      item.vegetables.distanceBetweenBushes
+                    ) /
+                      100) *
+                    0.01
+                  ).toFixed(1)
                 }}
                 соток )
               </div>
             </div>
             <div class="row">
               <div>В одном ряду:</div>
-              <div>{{ oneRows }} кустов</div>
+              <div>{{ oneRows(item.bushes, item.rows) }} кустов</div>
             </div>
           </div>
         </div>
@@ -96,61 +145,93 @@ import axios from "axios";
 export default {
   data() {
     return {
-      history: [
-        {
-          id: 1,
-          exp: false,
-          title: "Грядка в полесаднике рядом со входом слева от калитки",
-          vegetable: "Томаты",
-          bushes: 15,
-          sort: "Столыпин",
-          rows: 2,
-          distanceBetweenRows: 60,
-          distanceBetweenBushes: 50,
-          oneRows: 7,
-          width: 1400,
-          height: 1800,
-          dateAdd: "21 Января 2020",
-        },
-        {
-          id: 2,
-          exp: false,
-          title: "Грядка у уличного туалета в тени от сарая слева",
-          vegetable: "Огурцы",
-          bushes: 25,
-          sort: "Пальчик",
-          rows: 5,
-          distanceBetweenRows: 60,
-          distanceBetweenBushes: 50,
-          oneRows: 7,
-          width: 1300,
-          height: 1700,
-          dateAdd: "22 Января 2020",
-        },
-      ],
+      errorDataApi: false,
+      loadingDataApi: true,
+      history: null,
+
+      // history: [
+      //   {
+      //     id: 1,
+      //     exp: false,
+      //     title: "Грядка в полесаднике рядом со входом слева от калитки",
+      //     vegetable: "Томаты",
+      //     bushes: 15,
+      //     sort: "Столыпин",
+      //     rows: 2,
+      //     distanceBetweenRows: 60,
+      //     distanceBetweenBushes: 50,
+      //     oneRows: 7,
+      //     width: 1400,
+      //     height: 1800,
+      //     dateAdd: "21 Января 2020",
+      //   },
+      //   {
+      //     id: 2,
+      //     exp: false,
+      //     title: "Грядка у уличного туалета в тени от сарая слева",
+      //     vegetable: "Огурцы",
+      //     bushes: 25,
+      //     sort: "Пальчик",
+      //     rows: 5,
+      //     distanceBetweenRows: 60,
+      //     distanceBetweenBushes: 50,
+      //     oneRows: 7,
+      //     width: 1300,
+      //     height: 1700,
+      //     dateAdd: "22 Января 2020",
+      //   },
+      // ],
     };
   },
   mounted() {
     axios({
-      method: "put",
-      url: "http://127.0.0.1:8000/api/vegetable/7",
+      method: "GET",
+      url: "http://127.0.0.1:8000/api/V1/vegetable-calculate",
       params: {
         //user_key_id: "USER_KEY_ID",
       },
-      data: {
-        name: "Тыква",
-      },
+      data: {},
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
     })
-      .then(function (response) {
-        console.log("Ответ сервера успешно получен!");
-        console.log(response.data);
+      .then((response) => {
+        this.history = response.data.data;
+        console.log(response.data.data);
       })
-      .catch(function (error) {
+      .catch((error) => {
+        this.errorDataApi = true;
         console.log(error);
+      })
+      .finally(() => {
+        this.loadingDataApi = false;
       });
+  },
+  methods: {
+    /**
+     * Возвращает количество кустов одном ряду
+     * @param {*} bushes
+     * @param {*} rows
+     */
+    oneRows(bushes, rows) {
+      return Math.ceil(bushes / rows);
+    },
+    /**
+     * Расчитывает ширину грядки
+     * @param {*} rows
+     * @param {*} distanceBetweenRows
+     */
+    widh(rows, distanceBetweenRows) {
+      return rows * distanceBetweenRows + distanceBetweenRows;
+    },
+    /**
+     * Расчитывает длину грядки
+     * @param {*} oneRows
+     * @param {*} distanceBetweenBushes
+     */
+    height(oneRows, distanceBetweenBushes) {
+      return oneRows * distanceBetweenBushes + distanceBetweenBushes;
+    },
   },
 };
 </script>
