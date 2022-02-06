cfab9aa7b451bd18678798d2e6aa1100aceb0b47
kochenov <kochenof@gmail.com>
2022-02-06 15:31:02 +0300

    Реименование компонента меню

--- src/layouts/IndexLayout.vue
+++ src/layouts/IndexLayout.vue
@@ -1,33 +1,35 @@
 <template>
-  <header class="header">
-    <div class="wrap header__wrap">
-      <div class="header__logo-wrap">
-        <div class="logo">
-          <a class="logo-link" href="#">
-            <img src="/img/logo.png" alt="" />
-          </a>
-          <span>УСАДЬБА МЕЧТЫ</span>
+  <div>
+    <header class="header">
+      <div class="wrap header__wrap">
+        <div class="header__logo-wrap">
+          <div class="logo">
+            <a class="logo-link" href="#">
+              <img src="/img/logo.png" alt="" />
+            </a>
+            <span>УСАДЬБА МЕЧТЫ</span>
+          </div>
+        </div>
+        <div class="header__nav-wrap">
+          <MainMenu />
         </div>
       </div>
-      <div class="header__nav-wrap">
-        <Menu />
-      </div>
-    </div>
-  </header>
+    </header>
 
-  <PromoLink />
-  <Breadcrumbs />
-  <router-view />
+    <PromoLink />
+    <Breadcrumbs />
+    <router-view />
+  </div>
 </template>
 <script>
-import Menu from "@/components/main/Menu";
+import MainMenu from "@/components/main/MainMenu";
 import PromoLink from "@/components/main/PromoLink";
 import Breadcrumbs from "@/components/main/Breadcrumbs";
 
 export default {
   name: "IndexLayout",
   components: {
-    Menu,
+    MainMenu,
     PromoLink,
     Breadcrumbs,
   },
